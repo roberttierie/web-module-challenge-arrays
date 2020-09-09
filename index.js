d6aea9ffde5e82623e7fa5275ffd1e90bdd04a91
Robert Tierie <tangodolphin13@gmail.com>
2020-09-10 00:57:46 +0200

    extra extra done

--- index.js
+++ index.js
@@ -123,12 +123,12 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(x, y) {
-    let i = [...y];
-    return y
-} 
-    /*code here*/
+let newArray = []
 
+function copy(x, y) {
+    x = [...y];
+    return y
+} console.log(originalFlavors, newArray)
 
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
